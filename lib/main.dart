4b3e2c7b3c21d29a2004f12c1649ec1fd1eb7a93
Yago Maksuell Lacerda Cardoso <yago.cardoso@linx.com.br>
2023-03-20 20:49:21 -0300

    ajuste na classe scanner, Contexto inválido ao chamar Navigator.push. Feito acorreçao usando um Builder para obter um contexto interno ao MaterialApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'models/scanner.dart';
 
 void main() {
@@ -8,29 +9,33 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'QR Code Scanner',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('QR Code Scanner'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              final result = await Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Scanner()));
-              if (result != null) {
-                print('QR code lido: $result');
-              }
-            },
-            child: const Text('Iniciar Scanner'),
-          ),
-        ),
+      home: Builder(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('QR Code Scanner'),
+            ),
+            body: Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  final result = await Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Scanner()));
+                  if (result != null) {
+                    print('QR code lido: $result');
+                  }
+                },
+                child: const Text('Iniciar Scanner'),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
